ad3ed4b7a5800202b7623b90453265b37e50e7d0
Alan Paulino <alanvictor5447@gmail.com>
2019-08-27 08:53:55 -0300

    Merge feature profile on new Architecture

---
+++ Sigma/App/Features/Profile/Model/PreferredLanguagesCollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  PreferredLanguagesCollectionViewCell.swift
+//  Sigma
+//
+//  Created by Alan Victor Paulino de Oliveira on 26/08/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+class PreferredLanguagesCollectionViewCell: UICollectionViewCell {
+    let textLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce mollis tortor ullamcorper sapien eleifend faucibus. Cras malesuada lorem lectus, porttitor posuere nunc efficitur vitae. Donec laoreet arcu ligula, vitae faucibus nisi tincidunt eu. Aenean magna diam, interdum eu tincidunt et, sodales molestie elit."
+        label.font = UIFont(font: .boldSystem, size: 16)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(textLabel)
+        setupConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupConstraints() {
+        textLabel.fillSuperview(padding: UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8))
+    }
+}
