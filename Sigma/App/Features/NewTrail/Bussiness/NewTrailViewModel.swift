bfe785559716ad6e82dc318164647efe95e5b29a
Vinicius Mangueira <viniciuscorreia11@gmail.com>
2019-09-07 10:33:45 -0300

    Create implement view model and service to add trail

---
+++ Sigma/App/Features/NewTrail/Bussiness/NewTrailViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  NewTrailViewModel.swift
+//  Sigma
+//
+//  Created by Vinicius Mangueira on 07/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import Foundation
