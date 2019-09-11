aa4563e0aeb0abb1ba7963dfd7a96cc63e6a70c8
Alan Paulino <alanvictor5447@gmail.com>
2019-09-11 09:30:46 -0300

    Change the import foundation to UIkit in containerrow

--- Sigma/App/Features/Profile/View/ContainerRowCollectionViewCell.swift
+++ Sigma/App/Features/Profile/View/ContainerRowCollectionViewCell.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 Vinicius Mangueira. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class ContainerRowCollectionViewCell: UICollectionViewCell {
     let collection:UICollectionView = {
