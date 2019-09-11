b30fad9f4b64681549179b13588095eb17cf07f9
Alan Paulino <alanvictor5447@gmail.com>
2019-09-11 09:32:30 -0300

    create CustomScrollDelegate

---
+++ Sigma/App/Features/Profile/Define/CustomScrollDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CustomScrollDelegate.swift
+//  Sigma
+//
+//  Created by Alan Victor Paulino de Oliveira on 11/09/19.
+//  Copyright © 2019 Vinicius Mangueira. All rights reserved.
+//
+
+import UIKit
+
+protocol CustomScrollDelegate: class {
+    func scrollDidScroll(withOffset offset:CGPoint)
+}
